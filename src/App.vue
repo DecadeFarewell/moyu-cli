e85e8524c038b9902e08d395642c94d29d7bb156
guoxi <guoxi@shencom.cn>
2020-09-01 17:35:46 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,13 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
   font-size: 0.16rem;
+  text-align: center;
 }
 
 #nav a {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,60 @@
 <template>
   <div class="about">
-    <h1>This is an about page 1234</h1>
-    <el-button>按钮</el-button>
+    <div class="content">
+      <el-form class="el-from" ref="form" :model="form" label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 @Component
 export default class ComponetName extends Vue {
-  mounted() {
-    // document.execCommand("SaveAs");
-    document.execCommand('CreateLink', false, 'www.baidu.com');
-    console.log(111);
+  form = {
+    name: '',
+    name1: '',
+    age: '',
+    hobby: '',
+    region: '',
+    date1: '',
+    date2: '',
+    delivery: false,
+    type: [],
+    resource: '',
+    desc: '',
+  };
+
+  onSubmit() {
+    console.log('submit!');
   }
 }
 </script>
 <style lang="scss" scoped>
 .about {
   font-size: 0.16rem;
+  width: 100vw;
+  height: 90vh;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+  background: rgba($color: #ca7f7f, $alpha: 0.5);
+  overflow: hidden;
+  .content {
+    width: 80%;
+    height: 40%;
+    margin: 0 auto;
+    // margin-top: 700px;
+    margin-top: 90%;
+    background: #fff;
+    .el-form {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: flex-end;
+    }
+  }
 }
 </style>
