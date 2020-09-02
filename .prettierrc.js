9eadc9ca8fdfbf8b7e81a61a1d913532c9dac71e
guoxi <guoxi@shencom.cn>
2020-09-02 10:28:04 +0800

    fix: 修改prettier配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   eslintIntegration: true, // 是否支持eslint
   semi: true, // 是否加分号
-  // printWidth: 100, // 限制一行多大
+  printWidth: 100, // 限制一行多大
   trailingComma: 'all', // 对象尾随逗号
   singleQuote: true, // 单引号
   // arrowParens: 'always', // 单个箭头函数参数周围加上括号
