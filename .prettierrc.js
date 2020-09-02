cd712f3e0166315aa2c910159e5cb22d9047a2ac
guoxi <guoxi@shencom.cn>
2020-09-02 17:43:30 +0800

    feat: 初始化

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,6 +4,6 @@ module.exports = {
   printWidth: 100, // 限制一行多大
   trailingComma: 'all', // 对象尾随逗号
   singleQuote: true, // 单引号
-  // arrowParens: 'always', // 单个箭头函数参数周围加上括号
+  arrowParens: 'always', // 单个箭头函数参数周围加上括号
   jsxSingleQuote: true, // 在JSX中使用单引号
 };
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const webpack = require('webpack');
 const isDev = process.env.NODE_ENV === 'development';
+const resolve = (dir) => path.join(__dirname, dir);
 // import autoprefixer from "autoprefixer";
 // import pxtorem from "postcss-pxtorem";
 const pxtorem = require('postcss-pxtorem');
@@ -7,6 +10,26 @@ const pxtorem = require('postcss-pxtorem');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
+  // 扩展webpack配置
+  configureWebpack: (config) => {
+    // do sth...
+  },
+
+  // 对内部的 webpack 配置进行更细粒度的修改。
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('./src'))
+      .set('@c', resolve('./src/components'))
+      .set('@scss', resolve('./src/styles'))
+      .set('common', resolve('./src/common'));
+    // 自动加载模块，而不必到处 import 或 require
+    config.plugin('provide').use(
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jquery: 'jquery',
+      }),
+    );
+  },
   css: {
     sourceMap: isDev,
     loaderOptions: {
